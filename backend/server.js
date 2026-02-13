d1886dac9f01924abe36e608c8f41d8ec80a9bd6
codabishekk <abishekkc923@gmail.com>
2026-02-13 16:12:45 +0530

    feat: Add new backend server, frontend configuration, and install dependencies, while removing .prettierignore.

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,13 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
+import dotenv from 'dotenv'
+import app from './app.js'
+import connectDB from './src/config/db.js'
 
-const app = express()
+dotenv.config()
+
 const PORT = process.env.PORT || 5000
 
-// Middleware
-app.use(cors())
-app.use(express.json())
-
-// Basic Route
-app.get('/', (req, res) => {
-  res.send('Nxt Trends Backend is running!')
-})
+// Connect to Database
+connectDB()
 
 // Start Server
 app.listen(PORT, () => {
